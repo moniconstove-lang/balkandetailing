c16959348904024d2168ec5babf51f371a7e4787
moniconstove-lang <monicon.stove@gmail.com>
2025-09-15 23:58:54 +1200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+base: '/balkan-shine-builders/',
   server: {
     host: "::",
     port: 8080,
